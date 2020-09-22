7d457caac621f62c2789307322c3cc2bfc29f63e
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2020-09-22 23:48:55 +0200

    Added Shutdown Rpi function

--- usr/local/bin/RPiRadio.py
+++ usr/local/bin/RPiRadio.py
@@ -5,9 +5,10 @@ import time
 import dbus
 import os
 import subprocess
-from sh import bluetoothctl ## sh needs to be installed as root
+from sh import bluetoothctl
 
 pin = 15
+PowerOffPin = 3
 btMac= "FC:58:FA:82:B0:EC"
 btMac= "88:C6:26:50:81:A0"
 port = 1
@@ -21,41 +22,25 @@ def button_callback(channel):
 def ConnectToBTSpeaker():
     #Attempt 2
     bluetoothctl("connect", btMac)
-
-    #Attempt 3
-    #subprocess.call('bluetoothctl')
-    #subprocess.call('connect 88:C6:26:50:81:A0')
-
-    #Attempt 4
-    #https://blog.kevindoran.co/bluetooth-programming-with-python-3/
-serverMACAddress = '00:1f:e1:dd:08:3d'
-port = 3
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((serverMACAddress, port))
-while 1:
-    text = raw_input() # Note change to the old (Python 2) raw_input
-    if text == "quit":
-    break
-    s.send(text)
-sock.close()
-
-
-
+    return True
 
 def RestartMPD():
     print("Restarting MPD")
     os.system("sudo systemctl restart mpd.service")
-    """ sysbus = dbus.SystemBus()
-    systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
-    manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
-    job = manager.RestartUnit('mpd.service', 'fail') #Thank you https://stackoverflow.com/questions/33646374/starting-a-systemd-service-via-python
-    #hmm: https://zignar.net/2014/09/08/getting-started-with-dbus-python-systemd/ """
+    pass
+
+def ShutdownRpi(channel): #Based on https://scribles.net/adding-power-switch-on-raspberry-pi/
+    print("Shutdown Rpi, goodnight")
+    os.system("sudo shutdown -h now")
     pass
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(pin,GPIO.RISING,callback=button_callback,bouncetime=1000) #High bouncetime because this button is not Hulk-proof
+    
+    GPIO.setup(PowerOffPin, GPIO.IN)
+    GPIO.add_event_detect(PowerOffPin,GPIO.RISING,callback=ShutdownRpi,bouncetime=1000)
 
     try:
         while True:
