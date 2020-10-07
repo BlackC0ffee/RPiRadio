c2acb313261dcd0e42073a224cb79991109f2239
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2020-10-07 23:32:25 +0200

    Added a bouncer to prevent false positives

--- Rotary-Prototype/Rotary.py
+++ Rotary-Prototype/Rotary.py
@@ -3,10 +3,13 @@ import RPi.GPIO as GPIO
 
 # https://cdn.sparkfun.com/datasheets/Components/General/EC12PLGRSDVF.pdf
 # https://github.com/sparkfun/Rotary_Encoder_Breakout-Illuminated/blob/master/Firmware/RG_Rotary_Encoder/RG_Rotary_Encoder.ino
+# https://www.ozeki.hu/index.php?owpn=3054
 
 ROT_A=5
 ROT_B=6
 ROT_Counter=0
+ROT_Bounce=0
+ROT_Tolerance=2
 
 def setup():
     GPIO.setmode(GPIO.BCM)
@@ -17,11 +20,25 @@ def setup():
     pass
 
 def roteryIRQ(channel):
-
+    global ROT_Bounce
     if GPIO.input(ROT_A)==GPIO.input(ROT_B):
-        print("Left")
+        ROT_Bounce -= 1 #Move left detected
     else:
-        print("Right")
+        ROT_Bounce += 1 #Move right detected
+    CheckBounce()
+    print(ROT_Counter)
+    pass
+
+def CheckBounce(): #This prevents false positives
+    global ROT_Bounce, ROT_Counter
+    if ROT_Bounce == ROT_Tolerance:
+        ROT_Counter += 1 #Move counter up/right
+        ROT_Bounce = 0 # Reset our bounce
+        #pass
+
+    if ROT_Bounce == -ROT_Tolerance:
+        ROT_Counter -= 1 #Move counter up/right
+        ROT_Bounce = 0 # Reset our bounce
     pass
 
 if __name__ == "__main__":
