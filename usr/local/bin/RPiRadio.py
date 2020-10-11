521696c27d0ea19d960b8984cceaed7ba77b547e
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2020-10-11 23:16:40 +0200

    Added GPIO.PUD_DOWN to PowerOff function

--- usr/local/bin/RPiRadio.py
+++ usr/local/bin/RPiRadio.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     GPIO.setup(ConnectPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(ConnectPin,GPIO.RISING,callback=connect,bouncetime=1000) #High bouncetime because this button is not Hulk-proof
     
-    GPIO.setup(PowerOffPin, GPIO.IN)
+    GPIO.setup(PowerOffPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(PowerOffPin,GPIO.RISING,callback=shutdownRpi,bouncetime=1000)
 
     try:
